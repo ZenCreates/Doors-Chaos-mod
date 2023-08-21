aeff2d13c0fb0ccf7b7efc8b75ad16ed2130335d
ZenDev <76600019+ZenCreates@users.noreply.github.com>
2023-08-20 22:55:44 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,265 @@
+-- Instances: 21 | Scripts: 1 | Modules: 1
+local G2L = {};
+
+-- StarterGui.ChaosMod
+G2L["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
+G2L["1"]["IgnoreGuiInset"] = true;
+G2L["1"]["ScreenInsets"] = Enum.ScreenInsets.DeviceSafeInsets;
+G2L["1"]["Name"] = [[ChaosMod]];
+G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+
+-- StarterGui.ChaosMod.AlwaysOn
+G2L["2"] = Instance.new("Frame", G2L["1"]);
+G2L["2"]["ZIndex"] = 3;
+G2L["2"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["2"]["BackgroundTransparency"] = 1;
+G2L["2"]["Size"] = UDim2.new(1, 0, 1, 0);
+G2L["2"]["Name"] = [[AlwaysOn]];
+
+-- StarterGui.ChaosMod.AlwaysOn.Notifications
+G2L["3"] = Instance.new("Frame", G2L["2"]);
+G2L["3"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["3"]["BackgroundTransparency"] = 1;
+G2L["3"]["Size"] = UDim2.new(0.5984455943107605, 0, 0.866583526134491, 0);
+G2L["3"]["Selectable"] = true;
+G2L["3"]["Position"] = UDim2.new(0.23639896512031555, 0, 0.043640896677970886, 0);
+G2L["3"]["Name"] = [[Notifications]];
+
+-- StarterGui.ChaosMod.AlwaysOn.Notifications.UIListLayout
+G2L["4"] = Instance.new("UIListLayout", G2L["3"]);
+G2L["4"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
+
+-- StarterGui.ChaosMod.AlwaysOn.Notifications.WarningBase
+G2L["5"] = Instance.new("TextLabel", G2L["3"]);
+G2L["5"]["TextWrapped"] = true;
+G2L["5"]["TextScaled"] = true;
+G2L["5"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["5"]["FontFace"] = Font.new([[rbxasset://fonts/families/Oswald.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["5"]["TextSize"] = 14;
+G2L["5"]["TextColor3"] = Color3.fromRGB(255, 176, 63);
+G2L["5"]["Size"] = UDim2.new(0.9999999403953552, 0, 0.08536572009325027, 0);
+G2L["5"]["Text"] = [[Screech is active...]];
+G2L["5"]["Name"] = [[WarningBase]];
+G2L["5"]["Visible"] = false;
+G2L["5"]["BackgroundTransparency"] = 1;
+G2L["5"]["Position"] = UDim2.new(3.302768192270378e-08, 0, 0, 0);
+
+-- StarterGui.ChaosMod.AlwaysOn.Notification
+G2L["6"] = Instance.new("ModuleScript", G2L["2"]);
+G2L["6"]["Name"] = [[Notification]];
+
+-- StarterGui.ChaosMod.MainMenu
+G2L["7"] = Instance.new("Frame", G2L["1"]);
+G2L["7"]["BorderSizePixel"] = 0;
+G2L["7"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["7"]["Size"] = UDim2.new(1, 0, 1, 0);
+G2L["7"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["7"]["Visible"] = false;
+G2L["7"]["Name"] = [[MainMenu]];
+
+-- StarterGui.ChaosMod.MainMenu.UIGradient
+G2L["8"] = Instance.new("UIGradient", G2L["7"]);
+G2L["8"]["Rotation"] = 58;
+G2L["8"]["Color"] = ColorSequence.new{ColorSequenceKeypoint.new(0.000, Color3.fromRGB(163, 134, 101)),ColorSequenceKeypoint.new(1.000, Color3.fromRGB(87, 76, 63))};
+
+-- StarterGui.ChaosMod.MainMenu.Logo
+G2L["9"] = Instance.new("ImageLabel", G2L["7"]);
+G2L["9"]["BorderSizePixel"] = 0;
+G2L["9"]["ScaleType"] = Enum.ScaleType.Fit;
+G2L["9"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["9"]["Image"] = [[rbxassetid://14515639314]];
+G2L["9"]["Size"] = UDim2.new(0.29819878935813904, 0, 0.3753117322921753, 0);
+G2L["9"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["9"]["Name"] = [[Logo]];
+G2L["9"]["BackgroundTransparency"] = 1;
+G2L["9"]["Position"] = UDim2.new(0.015343562699854374, 0, 0.02244389057159424, 0);
+
+-- StarterGui.ChaosMod.MainMenu.Play
+G2L["a"] = Instance.new("TextButton", G2L["7"]);
+G2L["a"]["TextWrapped"] = true;
+G2L["a"]["BorderSizePixel"] = 0;
+G2L["a"]["TextScaled"] = true;
+G2L["a"]["BackgroundColor3"] = Color3.fromRGB(255, 214, 172);
+G2L["a"]["TextSize"] = 14;
+G2L["a"]["FontFace"] = Font.new([[rbxasset://fonts/families/Oswald.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["a"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["a"]["Size"] = UDim2.new(0.18745830655097961, 0, 0.09200000017881393, 0);
+G2L["a"]["Name"] = [[Play]];
+G2L["a"]["BorderColor3"] = Color3.fromRGB(240, 246, 177);
+G2L["a"]["Text"] = [[Play]];
+G2L["a"]["Position"] = UDim2.new(0.07100000232458115, 0, 0.42899999022483826, 0);
+
+-- StarterGui.ChaosMod.MainMenu.Play.UICorner
+G2L["b"] = Instance.new("UICorner", G2L["a"]);
+G2L["b"]["CornerRadius"] = UDim.new(0.5, 0);
+
+-- StarterGui.ChaosMod.MainMenu.Play.UIGradient
+G2L["c"] = Instance.new("UIGradient", G2L["a"]);
+G2L["c"]["Transparency"] = NumberSequence.new{NumberSequenceKeypoint.new(0.000, 0),NumberSequenceKeypoint.new(1.000, 0.12568306922912598)};
+G2L["c"]["Rotation"] = 35;
+G2L["c"]["Color"] = ColorSequence.new{ColorSequenceKeypoint.new(0.000, Color3.fromRGB(255, 255, 255)),ColorSequenceKeypoint.new(1.000, Color3.fromRGB(221, 221, 221))};
+
+-- StarterGui.ChaosMod.MainMenu.Settings
+G2L["d"] = Instance.new("TextButton", G2L["7"]);
+G2L["d"]["TextWrapped"] = true;
+G2L["d"]["BorderSizePixel"] = 0;
+G2L["d"]["TextScaled"] = true;
+G2L["d"]["BackgroundColor3"] = Color3.fromRGB(255, 214, 172);
+G2L["d"]["TextSize"] = 14;
+G2L["d"]["FontFace"] = Font.new([[rbxasset://fonts/families/Oswald.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["d"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["d"]["Size"] = UDim2.new(0.18745830655097961, 0, 0.09200000017881393, 0);
+G2L["d"]["Name"] = [[Settings]];
+G2L["d"]["BorderColor3"] = Color3.fromRGB(240, 246, 177);
+G2L["d"]["Text"] = [[Settings]];
+G2L["d"]["Position"] = UDim2.new(0.07071381062269211, 0, 0.5529999732971191, 0);
+
+-- StarterGui.ChaosMod.MainMenu.Settings.UICorner
+G2L["e"] = Instance.new("UICorner", G2L["d"]);
+G2L["e"]["CornerRadius"] = UDim.new(0.5, 0);
+
+-- StarterGui.ChaosMod.MainMenu.Settings.UIGradient
+G2L["f"] = Instance.new("UIGradient", G2L["d"]);
+G2L["f"]["Transparency"] = NumberSequence.new{NumberSequenceKeypoint.new(0.000, 0),NumberSequenceKeypoint.new(1.000, 0.12568306922912598)};
+G2L["f"]["Rotation"] = 35;
+G2L["f"]["Color"] = ColorSequence.new{ColorSequenceKeypoint.new(0.000, Color3.fromRGB(255, 255, 255)),ColorSequenceKeypoint.new(1.000, Color3.fromRGB(221, 221, 221))};
+
+-- StarterGui.ChaosMod.MainMenu.List
+G2L["10"] = Instance.new("TextButton", G2L["7"]);
+G2L["10"]["TextWrapped"] = true;
+G2L["10"]["BorderSizePixel"] = 0;
+G2L["10"]["TextScaled"] = true;
+G2L["10"]["BackgroundColor3"] = Color3.fromRGB(255, 214, 172);
+G2L["10"]["TextSize"] = 14;
+G2L["10"]["FontFace"] = Font.new([[rbxasset://fonts/families/Oswald.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["10"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["10"]["Size"] = UDim2.new(0.18700000643730164, 0, 0.09164588898420334, 0);
+G2L["10"]["Name"] = [[List]];
+G2L["10"]["BorderColor3"] = Color3.fromRGB(240, 246, 177);
+G2L["10"]["Text"] = [[List]];
+G2L["10"]["Position"] = UDim2.new(0.07071381062269211, 0, 0.671999990940094, 0);
+
+-- StarterGui.ChaosMod.MainMenu.List.UICorner
+G2L["11"] = Instance.new("UICorner", G2L["10"]);
+G2L["11"]["CornerRadius"] = UDim.new(0.5, 0);
+
+-- StarterGui.ChaosMod.MainMenu.List.UIGradient
+G2L["12"] = Instance.new("UIGradient", G2L["10"]);
+G2L["12"]["Transparency"] = NumberSequence.new{NumberSequenceKeypoint.new(0.000, 0),NumberSequenceKeypoint.new(1.000, 0.12568306922912598)};
+G2L["12"]["Rotation"] = 35;
+G2L["12"]["Color"] = ColorSequence.new{ColorSequenceKeypoint.new(0.000, Color3.fromRGB(255, 255, 255)),ColorSequenceKeypoint.new(1.000, Color3.fromRGB(221, 221, 221))};
+
+-- StarterGui.ChaosMod.MainMenu.Background
+G2L["13"] = Instance.new("ImageLabel", G2L["7"]);
+G2L["13"]["ZIndex"] = 0;
+G2L["13"]["BorderSizePixel"] = 0;
+G2L["13"]["BackgroundColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["13"]["Image"] = [[rbxassetid://14515777756]];
+G2L["13"]["Size"] = UDim2.new(1, 0, 1, 0);
+G2L["13"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["13"]["Name"] = [[Background]];
+
+-- StarterGui.ChaosMod.MainMenu.Background.UIGradient
+G2L["14"] = Instance.new("UIGradient", G2L["13"]);
+G2L["14"]["Transparency"] = NumberSequence.new{NumberSequenceKeypoint.new(0.000, 0),NumberSequenceKeypoint.new(1.000, 0.14754098653793335)};
+G2L["14"]["Rotation"] = 35;
+G2L["14"]["Color"] = ColorSequence.new{ColorSequenceKeypoint.new(0.000, Color3.fromRGB(116, 116, 116)),ColorSequenceKeypoint.new(1.000, Color3.fromRGB(255, 255, 255))};
+
+-- StarterGui.ChaosMod.MainMenu.Background.LocalScript
+G2L["15"] = Instance.new("LocalScript", G2L["13"]);
+
+
+-- Require G2L wrapper
+local G2L_REQUIRE = require;
+local G2L_MODULES = {};
+local function require(Module:ModuleScript)
+    local ModuleState = G2L_MODULES[Module];
+    if ModuleState then
+        if not ModuleState.Required then
+            ModuleState.Required = true;
+            ModuleState.Value = ModuleState.Closure();
+        end
+        return ModuleState.Value;
+    end;
+    return G2L_REQUIRE(Module);
+end
+
+G2L_MODULES[G2L["6"]] = {
+Closure = function()
+    local script = G2L["6"];
+local module = {}
+local notifs = script.Parent.Notifications
+local ts = game:GetService("TweenService")
+
+--Normal Notifs -----------------------------------------------------------------------------------------------------------
+module.Notif = function(Text, timebeforefade, fadetime)
+	local NewNotif = notifs.WarningBase:Clone()
+	NewNotif.Name = "Warning"
+	NewNotif.Text = Text
+	NewNotif.Visible = true
+	NewNotif.Parent = notifs
+	task.delay(timebeforefade, function()
+		ts:Create(NewNotif, TweenInfo.new(fadetime), {TextTransparency = 1}):Play()
+		task.delay(fadetime, function()
+			NewNotif:Destroy()
+		end)
+	end)
+end
+module.NotifShow = function(Text)
+	local NewNotif = notifs.WarningBase:Clone()
+	NewNotif.Name = Text
+	NewNotif.Text = Text
+	NewNotif.Visible = true
+	NewNotif.Parent = notifs
+end
+
+module.NotifHide = function(Name ,fadetime)
+	local NewNotif = notifs[Name]
+	ts:Create(NewNotif, TweenInfo.new(fadetime), {TextTransparency = 1}):Play()
+	task.delay(fadetime, function()
+		NewNotif:Destroy()
+	end)
+end
+
+return module
+
+end;
+};
+-- StarterGui.ChaosMod.MainMenu.Background.LocalScript
+local function C_15()
+local script = G2L["15"];
+	local backgrounds = {
+		"rbxassetid://14515693977",
+		"rbxassetid://14515760224",
+		"rbxassetid://14515777756"
+	}
+	local ts = game:GetService("TweenService")
+	
+	script.Parent.Visible = true
+	script.Parent.Size = UDim2.new(3,0,3,0)
+	script.Parent.Position = UDim2.new(-1, 0,-1, 0)
+	script.Parent.Image = backgrounds[math.random(1,#backgrounds)]
+	script.Parent.Rotation = 35
+	script.Parent.ImageColor3 = Color3.new(0, 0, 0)
+	local buttons = script.Parent.Parent
+	buttons.Play.Position = UDim2.new(0.071, 0, buttons.Play.Position.Y.Scale + 1.429, 0)
+	buttons.Settings.Position = UDim2.new(0.071, 0, buttons.Settings.Position.Y.Scale + 1.553, 0)
+	buttons.List.Position = UDim2.new(0.071, 0, buttons.List.Position.Y.Scale + 1.672, 0)
+	buttons.Logo.ImageTransparency = 1
+	--if game:GetService("RunService"):IsStudio() then
+	--	wait(3)
+	--end
+	ts:Create(script.Parent, TweenInfo.new(3, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {Size = UDim2.new(1,0,1,0), Position = UDim2.new(0,0,0,0), Rotation = 0, ImageColor3 = Color3.new(1, 1, 1)}):Play()
+	ts:Create(buttons.Logo, TweenInfo.new(3, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {ImageTransparency = 0}):Play()
+	wait(0.5)
+	ts:Create(buttons.Play, TweenInfo.new(1, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {Position = UDim2.new(0.071, 0, 0.429, 0)}):Play()
+	task.wait(0.1)
+	ts:Create(buttons.Settings, TweenInfo.new(1, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {Position = UDim2.new(0.071, 0, 0.553, 0)}):Play()
+	task.wait(0.1)
+	ts:Create(buttons.List, TweenInfo.new(1, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {Position = UDim2.new(0.071, 0, 0.672, 0)}):Play()
+	
+	
+end;
+task.spawn(C_15);
+
+return G2L["1"], require;
