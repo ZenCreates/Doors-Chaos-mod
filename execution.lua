0c3f7b965185a9fc174f9d75432f2845ca3344bd
ZenDev <76600019+ZenCreates@users.noreply.github.com>
2023-08-20 22:58:47 -0700

    Create execution.lua

---
+++ execution.lua
@@ -0,0 +1,4 @@
+pcall(function()
+    game.Players.LocalPlayer.PlayerGui.ChaosMod:Destroy()
+end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZenCreates/Doors-Chaos-mod/main/main.lua"))()
